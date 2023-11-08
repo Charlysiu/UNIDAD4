bd94bdf2dafb67091456bba33fb4b3870c7e5976
Charlysiu <143622334+Charlysiu@users.noreply.github.com>
2023-11-07 19:56:06 -0600

    Add files via upload

---
+++ Arreglo Unidad 4.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) {
+    string[][] arreglo = new string[3][5];                  
+        arreglo[0] = new string[] {'a', 'b', 'c', 'd', 'e'};         
+        arreglo[1] = new string[] {'f', 'g', 'h', 'i', 'j'};         
+        arreglo[2] = new string[] {'k', 'I', 'm', 'n', 'o'};                  
+        for (int i = 0; i < arreglo; i++) {             
+            for (int j = 0; j < arreglo[i]; j++) {                
+                
+        System.out.print(arreglo[i][j]);             }             
+        System.out.println();        
+        }    
+    } 
+}
+    }
+}
